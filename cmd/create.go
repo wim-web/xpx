7d316900121830b177a845d2cb95189a6965b6c2
wim-web <45655999+wim-web@users.noreply.github.com>
2023-04-09 11:44:06 +0900

    cloudformationからリソースを作成する

--- cmd/create.go
+++ cmd/create.go
@@ -6,10 +6,14 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	c_types "github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/ktr0731/go-fuzzyfinder"
@@ -79,9 +83,69 @@ var createCmd = &cobra.Command{
 		if len(res2.InternetGateways) < 1 {
 			log.Fatalln("selected vpc have no internet gateway")
 		}
+
+		client := cloudformation.NewFromConfig(cfg)
+
+		createFromStack(client)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(createCmd)
+}
+
+func createFromStack(svc *cloudformation.Client) {
+	stackName := "temp"
+	templateFile := "network.yaml"
+
+	template, err := ioutil.ReadFile(templateFile)
+	if err != nil {
+		log.Fatalf("unable to read CloudFormation template file, %v", err)
+	}
+
+	input := &cloudformation.CreateStackInput{
+		StackName:    aws.String(stackName),
+		TemplateBody: aws.String(string(template)),
+	}
+
+	_, err = svc.CreateStack(context.Background(), input)
+	if err != nil {
+		log.Fatalf("unable to create stack, %v")
+	}
+
+	fmt.Printf("Stack %s has been created successfully.\n", stackName)
+
+	waitForStackCompletion(svc, stackName)
+
+	fmt.Printf("Stack %s has been created successfully.\n", stackName)
+}
+
+func waitForStackCompletion(client *cloudformation.Client, stackName string) {
+	fmt.Printf("Waiting for stack %s to complete...\n", stackName)
+
+	describeStacksInput := &cloudformation.DescribeStacksInput{
+		StackName: aws.String(stackName),
+	}
+
+	for {
+		resp, err := client.DescribeStacks(context.Background(), describeStacksInput)
+		if err != nil {
+			log.Fatalf("unable to describe stack, %v", err)
+		}
+
+		stack := resp.Stacks[0]
+
+		switch stack.StackStatus {
+		case c_types.StackStatusCreateComplete:
+			return
+		case c_types.StackStatusCreateInProgress:
+			// Continue polling
+		default:
+			log.Fatalf("Stack creation failed with status: %s", stack.StackStatus)
+		}
+
+		// Sleep for a while before polling again
+		fmt.Println("!")
+		time.Sleep(3 * time.Second)
+	}
 }
